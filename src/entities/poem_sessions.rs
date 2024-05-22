e6ec5380be04e7a418ce791f3657caf4da74d1ee
887 <2300887@gmail.com>
2024-05-23 01:15:44 +0200

    cleanup

--- src/entities/poem_sessions.rs
+++ src/entities/poem_sessions.rs
@@ -1,6 +1,5 @@
-use sea_orm::entity::prelude::*;
-// use serde::{Deserialize, Serialize};
 use chrono::{DateTime, Utc};
+use sea_orm::entity::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
 #[sea_orm(table_name = "poem_sessions")]
--- src/migration/m20230512_212949_poem_session.rs
+++ src/migration/m20230512_212949_poem_session.rs
@@ -7,13 +7,6 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // let builder = manager.get_database_backend();
-        // let schema = sea_orm::Schema::new(builder);
-        // let entity = poem_sessions::Entity;
-        // manager
-        //     .create_table(schema.create_table_from_entity(entity))
-        //     .await
-
         let db = manager.get_connection();
         let backend = db.get_database_backend();
 
@@ -52,14 +45,8 @@ impl MigrationTrait for Migration {
             .drop_table(Table::drop().table(PoemSessions::Table).to_owned())
             .await
     }
-    // async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-    //     manager
-    //         .drop_table(Table::drop().table(PoemSessions::Table).to_owned())
-    //         .await
-    // }
 }
 
-// /// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
 #[iden = "poem_sessions"]
 enum PoemSessions {
